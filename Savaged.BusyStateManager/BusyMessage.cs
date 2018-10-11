6b7fd76852788b57161214bc41959d39b7e369f8
savaged <david@savaged.info>
2018-10-11 10:26:18 +0100

    initial commit

---
+++ Savaged.BusyStateManager/BusyMessage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace Savaged.BusyStateManager
+{
+    /// <summary>
+    /// Message for updating Busy State, used in conjunction with 
+    /// BusyStateRegistry in this same library
+    /// </summary>
+    /// <example>
+    /// Used in conjunction with BusyStateRegistry in this library
+    /// like this:
+    /// <code>
+    /// MessengerInstance.Send(new BusyMessage(true, this));
+    /// await DoSomthingAsync();
+    /// MessengerInstance.Send(new BusyMessage(false, this));
+    /// </code>
+    /// </example>
+    public class BusyMessage : MessageBase
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="isBusy">The state</param>
+        /// <param name="caller">The calling class</param>
+        /// <param name="callerMember">
+        /// The method or property (leave blank because reflection gets this)
+        /// </param>
+        public BusyMessage(bool isBusy, object caller, [CallerMemberName] string callerMember = "")
+        {
+            IsBusy = isBusy;
+            CallerType = caller.GetType();
+            CallerMember = callerMember;
+        }
+
+        public bool IsBusy { get; }
+
+        public Type CallerType { get; }
+
+        public string CallerMember { get; }
+    }
+}
---
+++ Savaged.BusyStateManager/BusyStateRegistry.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace Savaged.BusyStateManager
+{
+    /// <summary>
+    /// The main class that provides a Boolean value for busy which should
+    /// be used in conjunction with the Xceed Toolkit BusyIndicator or 
+    /// similar control. This class is responsible for managing the busy
+    /// state which can be problematic if it is just being set in process
+    /// because different threads can set it prior to other threads 
+    /// completing. This class aims to provide a better solution, namely
+    /// to have this central static register of running threads, which 
+    /// unregister once complete, then the busy value is only set to 
+    /// true once the register is empty.
+    /// </summary>
+    public class BusyStateRegistry : ObservableObject
+    {
+        private IList<string> _registry;
+
+        public BusyStateRegistry()
+        {
+            _registry = new List<string>();
+            Messenger.Default.Register<BusyMessage>(this, OnBusyMessage);
+        }
+
+        public bool IsBusy => _registry.Count > 0;
+
+        private void OnBusyMessage(BusyMessage m)
+        {
+            if (m.IsBusy)
+            {
+                _registry.Add($"{m.CallerType}.{m.CallerMember}");
+            }
+            else
+            {
+                _registry.Remove($"{m.CallerType}.{m.CallerMember}");
+            }
+            RaisePropertyChanged(nameof(IsBusy));
+        }
+    }
+}
